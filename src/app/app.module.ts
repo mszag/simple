4fe4a519feaa841e1c05214d53de15df2de655d8
Zagdan <MZagdan@corp.altegrity.com>
2017-02-24 15:28:24 +0100

    added component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SimpleComponentComponent } from './simple-component/simple-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleComponentComponent
   ],
   imports: [
     BrowserModule,
